9161b098cc8f0d83365582c170c9c61415c961d3
Mohammed Sufyan <sufyan297@gmail.com>
2021-02-10 10:20:21 +0530

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import LoginContainer from '../components/login/LoginContainer';
-
-export default class Login extends React.Component {
+import withLoginLayout from '../layouts/withLoginLayout';
+class Login extends React.Component {
   render() {
     return (
       <div>
@@ -10,3 +10,4 @@ export default class Login extends React.Component {
     )
   }
 }
+export default withLoginLayout(Login, { title: 'Login'});
